d317c08e73068f89478615905916e505a2476b3f
Sij <sijibamgboye@yahoo.com>
2021-09-13 21:14:57 +0100

    instructurs solution for rooks are safe

--- 2darrays/rooks_are_safe.py
+++ 2darrays/rooks_are_safe.py
@@ -1,4 +1,7 @@
-chessboard =  [[0,1,0,0],[0,0,1,0],[0,0,0,0],[0,0,0,1] ]
+chessboard = [[1,0],[0,0]]
+
+
+#  [[0,1,0,0],[0,0,1,0],[0,0,0,0],[0,0,0,1] ]
 
 
 
@@ -15,3 +18,25 @@ def rooks_are_safe(array):
     return True
 
 print(rooks_are_safe(chessboard))
+
+def rooks_are_safe2(chessboard):
+    n = len(chessboard)
+
+    for row_i in range(n):
+        row_count = 0
+        for col_i in range(n):
+            row_count += chessboard[row_i][col_i]
+        if row_count > 1:
+            return False 
+    
+    for col_i in range(n):
+        col_count = 0
+        for row_i in range(n):
+            col_count += chessboard[row_i][col_i]
+        if col_count > 1:
+            return False
+    
+    return True
+
+# Instucture's code above. Totally prefer mine rooks_are_safe()
+print(rooks_are_safe2(chessboard))
